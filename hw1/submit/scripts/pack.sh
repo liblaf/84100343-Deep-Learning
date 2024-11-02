01bc56984a24a1bc115b7f118aaea7e5e2a0fe91
liblaf <30631553+liblaf@users.noreply.github.com>
2024-11-02 17:15:55 +0800

    chore: include additional README files in submission package
    This change ensures that all relevant documentation is included when generating the submission package, improving the completeness of the submission process.

--- hw1/submit/scripts/pack.sh
+++ hw1/submit/scripts/pack.sh
@@ -6,7 +6,7 @@ set -o pipefail
 tmpdir=$(mktemp --directory)
 trap 'rm --force --recursive -- "$tmpdir"' EXIT
 readarray -t files < <(git ls-files)
-# files+=(part1/README.pdf part2/README.pdf)
+files+=(part1/README.pdf part2/README.pdf)
 for file in "${files[@]}"; do
   mkdir --parents -- "$tmpdir/$(dirname -- "$file")"
   cp --archive -- "$file" "$tmpdir/$file"
