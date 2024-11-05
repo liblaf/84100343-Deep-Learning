f841ea8bc43b04effd9ecde555ded147b5839b9c
liblaf <30631553+liblaf@users.noreply.github.com>
2024-11-05 20:48:48 +0800

    chore: include project configuration files in submission script
    Ensures that the latest project dependencies and configurations are included in the submission package.

--- hw1/submit/scripts/pack.sh
+++ hw1/submit/scripts/pack.sh
@@ -3,12 +3,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+git_root=$(git rev-parse --show-toplevel)
 tmpdir=$(mktemp --directory)
 trap 'rm --force --recursive -- "$tmpdir"' EXIT
 readarray -t files < <(git ls-files)
-files+=(part1/README.pdf part2/README.pdf part3/README.pdf)
+files+=(
+  part1/README.pdf
+  part2/README.pdf
+  part3/README.pdf
+)
 for file in "${files[@]}"; do
   mkdir --parents -- "$tmpdir/$(dirname -- "$file")"
   cp --archive -- "$file" "$tmpdir/$file"
 done
+cp --archive -- "$git_root/pixi.lock" "$tmpdir/pixi.lock"
+cp --archive -- "$git_root/pyproject.toml" "$tmpdir/pyproject.toml"
 ouch compress "$tmpdir"/* submit.zip
